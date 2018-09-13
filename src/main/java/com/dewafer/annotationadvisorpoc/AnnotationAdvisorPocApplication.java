dd4dfaa1b12994a573ee7e133c7abfb1efcfd1b2
Wang, Yinqiu <yinqiu.wang@rakuten.com>
2018-09-13 13:30:07 +0800

    first commit

---
+++ src/main/java/com/dewafer/annotationadvisorpoc/AnnotationAdvisorPocApplication.java
@@ -0,0 +1,12 @@
+package com.dewafer.annotationadvisorpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationAdvisorPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationAdvisorPocApplication.class, args);
+	}
+}
